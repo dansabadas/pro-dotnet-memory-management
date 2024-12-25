8aab7c21cc5299d32858279f7bc35acc2a123f67
STRATEGIKON-DAN\dan_s <dsabadas@strategikonpharma.com>
2024-12-25 22:19:17 +0200

    arrived at pg 49 Tracking collector

--- C/helloworld.cpp
+++ C/helloworld.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <memory>
 
 using namespace std;
+
+void printReport(int* data)
+{
+    std::cout << "Report: " << *data << "\n";
+}
+
+void printReport2(std::shared_ptr<int> data)
+{
+    std::cout << "Report: " << data.use_count() << " " << *data << "\n";
+}
 
 int main()
 {
@@ -13,4 +24,33 @@ int main()
         cout << word << " ";
     }
     cout << endl;
+
+    try
+    {
+        int* ptr;
+        ptr = new int();
+        *ptr = 25;
+        printReport(ptr);
+        delete ptr;
+        ptr = 0;
+        //return 0;
+    }
+    catch (std::bad_alloc& ba)
+    {
+        std::cout << "ERROR: Out of memory\n";
+        return 1;
+    }
+
+    try
+    {
+        std::shared_ptr<int> ptr2(new int());
+        *ptr2 = 25;
+        printReport2(ptr2);
+        return 0;
+    }
+    catch (std::bad_alloc& ba)
+    {
+        std::cout << "ERROR: Out of memory\n";
+        return 1;
+    }
 }
